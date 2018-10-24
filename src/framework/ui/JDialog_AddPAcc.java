bc79eb1978731c7489356b165c43d2e025fe98aa
kos602 <kalmurzaev.oljas@gmail.com>
2018-10-23 22:20:19 -0500

    gui fields fix

--- src/framework/ui/JDialog_AddPAcc.java
+++ src/framework/ui/JDialog_AddPAcc.java
@@ -17,7 +17,6 @@ import javax.swing.*;
 public class JDialog_AddPAcc extends JDialog
 {
     private MainScreen main;
-	private Object rowdata[];
     
 	public JDialog_AddPAcc(MainScreen main)
 	{
@@ -33,45 +32,46 @@ public class JDialog_AddPAcc extends JDialog
 		JLabel1.setText("Name");
 		getContentPane().add(JLabel1);
 		JLabel1.setForeground(Color.black);
-		JLabel1.setBounds(12,84,48,24);
+		JLabel1.setBounds(12,40,80,24);
 		JLabel2.setText("Street");
 		getContentPane().add(JLabel2);
 		JLabel2.setForeground(Color.black);
-		JLabel2.setBounds(12,108,48,24);
+		JLabel2.setBounds(12,70,80,24);
 		JLabel3.setText("City");
 		getContentPane().add(JLabel3);
 		JLabel3.setForeground(Color.black);
-		JLabel3.setBounds(12,132,48,24);
+		JLabel3.setBounds(12,100,80,24);
 		JLabel4.setText("State");
 		getContentPane().add(JLabel4);
 		JLabel4.setForeground(Color.black);
-		JLabel4.setBounds(12,156,48,24);
+		JLabel4.setBounds(12,130,80,24);
 		JLabel5.setText("Zip");
 		getContentPane().add(JLabel5);
 		JLabel5.setForeground(Color.black);
-		JLabel5.setBounds(12,180,48,24);
+		JLabel5.setBounds(12,160,80,24);
 		JLabel6.setText("Birthdate");
 		getContentPane().add(JLabel6);
 		JLabel6.setForeground(Color.black);
-		JLabel6.setBounds(12,204,96,24);
+		JLabel6.setBounds(12,190,80,24);
 		JLabel7.setText("Email");
 		getContentPane().add(JLabel7);
 		JLabel7.setForeground(Color.black);
-		JLabel7.setBounds(12,228,48,24);
+		JLabel7.setBounds(12,220,80,24);
 		getContentPane().add(JTextField_NAME);
-		JTextField_NAME.setBounds(84,84,156,20);
+		JTextField_NAME.setBounds(100,40,156,20);
 		getContentPane().add(JTextField_CT);
-		JTextField_CT.setBounds(84,132,156,20);
+		JTextField_CT.setBounds(100,70,156,20);
 		getContentPane().add(JTextField_ST);
-		JTextField_ST.setBounds(84,156,156,20);
+		JTextField_ST.setBounds(100,100,156,20);
 		getContentPane().add(JTextField_STR);
-		JTextField_STR.setBounds(84,108,156,20);
+		JTextField_STR.setBounds(100,130,156,20);
 		getContentPane().add(JTextField_ZIP);
-		JTextField_ZIP.setBounds(84,180,156,20);
+		JTextField_ZIP.setBounds(100,160,156,20);
 		getContentPane().add(JTextField_BD);
-		JTextField_BD.setBounds(84,204,156,20);
+		JTextField_BD.setBounds(100,190,156,20);
 		getContentPane().add(JTextField_EM);
-		JTextField_EM.setBounds(84,228,156,20);
+		JTextField_BD.setText("1/01/1990");
+		JTextField_EM.setBounds(100,220,156,20);
 
 		JButton_OK.setText("OK");
 		JButton_OK.setActionCommand("OK");
@@ -83,11 +83,11 @@ public class JDialog_AddPAcc extends JDialog
 		JButton_Cancel.setBounds(156,264,84,24);
 
 		getContentPane().add(JTextField_ACNR);
-		JTextField_ACNR.setBounds(84,60,156,20);
-		JLabel8.setText("Acc Nr");
+		JTextField_ACNR.setBounds(100,10,156,20);
+		JLabel8.setText("Account number");
 		getContentPane().add(JLabel8);
 		JLabel8.setForeground(Color.black);
-		JLabel8.setBounds(12,60,48,24);
+		JLabel8.setBounds(12,10,80,24);
 		//}}
 	
 		//{{REGISTER_LISTENERS
@@ -159,11 +159,11 @@ public class JDialog_AddPAcc extends JDialog
             newPerson.addAccount(account);
             accounts.add(account);
             main.updateTable();
+            dispose();
         } else {
             JOptionPane.showMessageDialog(this, "Cannot create account, account number already exists","Error!", JOptionPane.ERROR_MESSAGE);
         }
 
-        dispose();
 	}
 
 	void JButtonCalcel_actionPerformed(java.awt.event.ActionEvent event)
